c0d492c63a46295d5dedfed4f0c7630202f2e672
Nigar Ayla Ozcanan <nigarozcnn@gmail.com>
2024-10-03 16:30:22 +0300

    reorganized BaseDriver and Config. file

--- src/utilities/BaseDriver.java
+++ src/utilities/BaseDriver.java
@@ -10,12 +10,10 @@ import java.time.Duration;
 public class BaseDriver {
     public static WebDriver driver;
     public static WebDriverWait wait;
-    static int xPosition = ConfigReader.getIntProperty("windowPositionX");
-    static int yPosition = ConfigReader.getIntProperty("windowPositionY");
+
     static {
         driver=new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().window().setPosition(new Point(xPosition, yPosition));
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(ConfigReader.getIntProperty("pageLoadTimeout")));
         wait=new WebDriverWait(driver,Duration.ofSeconds(ConfigReader.getIntProperty("explicit.wait")));
     }
